779a986bd9439684b4cb38b6506ef6c68727629c
kiahmad <kiahmed-hyd@outlook.com>
2026-03-04 16:20:38 +0530

    todays mar4 2026

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from blog.models import Tag, Post, PostAdmin
 
 # Register your models here.
+
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+
+    
